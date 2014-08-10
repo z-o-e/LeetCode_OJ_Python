ba2fb104acf07df9e7830840012a08de8978f675
eeooz <hyzhang22@gmail.com>
2014-08-10 11:00:21 -0400

    dfs

---
+++ generate_parenthesis.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param an integer
+    # @return a list of string
+    def generateParenthesis(self, n):
+        self.res, self.n = [], n
+        
+        self.dfs('', 0, 0)
+        
+        return self.res
+        
+    def dfs(self, s, left, right):
+        if self.n==left==right:
+            self.res.append(s)
+            return 
+        elif left<self.n:
+            self.dfs(s+'(', left+1, right)
+        elif right<self.n:
+            self.dfs(s+')', left, right+1)
+            
+        return
+    
