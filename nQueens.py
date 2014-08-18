b98140dd9d422dc3fb40be7e6c62b5f04667d2d1
eeooz <hyzhang22@gmail.com>
2014-08-18 18:04:07 -0400

    dfs

--- nQueens.py
+++ nQueens.py
@@ -1,54 +1,36 @@
 class Solution:
     # @return a list of lists of string
     def solveNQueens(self, n):
-        if n==1:
-            return [['Q']]
-        if n<4:
-            return []
-            
         self.res = []
-        board = [ '.'*n for i in range(n) ]
         
-        for i in range(n):
-            tmp = board[:]
-            tmp[0] = '.'*i+'Q'+'.'*(n-i-1)
-            self.dp( tmp, 1, n, 0, i)
-                    
+        self.solver(0, [-1]*n, n)
+        
         return self.res
-        
-    def dp(self, board, Qnum, n, row, col):
-        #print board
-        if Qnum==n:
-            self.res.append(board)
-            return
-        
-        if row>n-1 or col>n-1:
+    
+    def solver(self, idx, board, n):
+        # current state board[i]=j indicates 'Q' placed at position i-th row, j-th column
+        if idx==n:
+            r = ['.'*n for i in range(n)]
+            for i in range(n):
+                if board[i]==n-1:
+                    r[i] = r[i][:n-1]+'Q'
+                else:
+                    r[i] = r[i][:board[i]]+'Q'+r[i][board[i]+1:]
+            self.res.append(r)
             return 
-            
-        tmp = board[:]   
-            
-        if 'Q' in board[row]:
-            if row<n-1:
-                self.dp( board, Qnum, n, row+1, 0)
         
-        elif 'Q' in ''.join(board[i][col] for i in range(row)):
-            if col<n-1:
-                self.dp(board, Qnum, n, row, col+1)
-            elif col==n-1 and row<n-1:
-                self.dp( board, Qnum, n, row+1, 0)
+        # for each position before n,
+        #   place queen
+        #   if current state is valid, then place next queen solver(depth+1, n)     
+        for i in range(n):
+            board[idx] = i
+            if (self.isValid(idx, board)):
+                # increment line
+                self.solver(idx+1, board, n)
                 
-        elif 'Q' in ''.join(board[row-i][col-i] for i in range(1,min(row, col)+1)) or 'Q' in ''.join(board[row-i][col+i] for i in range(1,min(row+1, n-col))) :
-            if col<n-1:
-                self.dp( board, Qnum, n, row, col+1)
-            elif col==n-1 and row<n-1:
-                self.dp( board, Qnum, n, row+1, 0)   
-        
-        else:
-            if col<=n-1:
-                board[row] = board[row][:col]+'Q'+board[row][col+1:]                                
-            else:
-                if row<n:
-                    board[row] = board[row][:col-1]+'Q'
-                
-            self.dp(board, Qnum+1, n, row+1, 0)  
-            self.dp(tmp, Qnum, n, row, col+1) 
+    def isValid(self, idx, board):
+        for i in range(idx):
+            if board[i]==board[idx] or abs(board[i]-board[idx])==abs(idx-i):
+                return False
+        return True
+            
