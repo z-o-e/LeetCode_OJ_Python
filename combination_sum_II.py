f1b9bfa7b23ea116ea6ea56749b2e16dd1d9bb3f
eeooz <hyzhang22@gmail.com>
2014-08-19 13:48:18 -0400

    dfs tle

---
+++ combination_sum_II.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param candidates, a list of integers
+    # @param target, integer
+    # @return a list of lists of integers
+    def combinationSum2(self, candidates, target):
+        candidates = sorted(candidates)
+        self.res = []
+        self.dfs(candidates, [], target)
+        
+        return self.res
+        
+    def dfs(self, candidates, cur, target):
+        if target==0:
+            if sorted(cur) not in self.res:
+                self.res.append(cur)
+            return 
+        
+        for c in candidates:
+            if target-c>=0:
+                tmp = cur[:]
+                tmp.append(c)
+                self.dfs(candidates[1:], tmp, target-c)
