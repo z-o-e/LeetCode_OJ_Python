e0f4ea9b0e8a0a6764e420d0d1d03af424feceb8
Zoe Zhang <hyzhang22@gmail.com>
2014-09-12 21:11:37 -0400

    dp

--- word_break.py
+++ word_break.py
@@ -1,23 +1,19 @@
+# dp ---O(n^2)time, O(n)space f(i) = any_of(f(j)&&s[j + 1, i] in dict), 0 <= j < i
+# dfs --- tle O(2^n)time, O(n)space
 class Solution:
     # @param s, a string
     # @param dict, a set of string
     # @return a boolean
     def wordBreak(self, s, dict):
+        sLen  = len(s)
+        f = [False for i in range(sLen+1)]
+        f[0] = True
         
-        return self.dfs(0,0,s,dict) 
-    
-    def dfs(self, preIdx, curIdx, s, dict):
-        if curIdx==len(s)-1:
-            if s[preIdx:curIdx+1] in dict:
-                return True
-            else:
-                return False
-                
-        if s[preIdx:curIdx+1] in dict:
-            if self.dfs(preIdx, curIdx+1, s, dict) or self.dfs(curIdx+1, curIdx+1, s, dict):
-                return True  
-        else:
-            return self.dfs(preIdx, curIdx+1, s, dict)
-            
+        for i in range(1, sLen+1):
+            for j in range(i):
 
-                        
+                if f[j] and s[j:i] in dict:
+                    f[i] = True
+                          
+        return f[-1]
+    
