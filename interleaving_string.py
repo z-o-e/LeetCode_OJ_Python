a1e7236bc13bf32ecddda333e094165a7c508b5b
Zoe Zhang <hyzhang22@gmail.com>
2014-10-03 13:54:59 -0700

    dp m*n solution

--- interleaving_string.py
+++ interleaving_string.py
@@ -3,21 +3,20 @@ class Solution:
     def isInterleave(self, s1, s2, s3):
         if len(s3)!=len(s1)+len(s2):
             return False
-        
-        p1, p2, i = 0, 0, 0
-        
-        while p1<len(s1) and p2<len(s2):
-            if s3[i]!=s1[p1] and s3[i]!=s2[p2]:
-                return False
-            elif s3[i]==s1[p1] and s3[i]!=s2[p2]:
-                p1 +=1
-                i += 1
-            elif s3[i]==s2[p2] and s3[i]!=s1[p1]:
-                p2 +=1
-                i += 1
-            else:
-                return self.isInterleave(s1[p1+1:], s2[p2:], s3[i+1:]) or self.isInterleave(s1[p1:], s2[p2+1:], s3[i+1:])
+        if not s1 and not s2 and not s3:
+            return True
+            
+        l1,l2 = len(s1), len(s2)
+        # D[i][j] = (s3[i+j]==s1[i] and D[i-1][j]) or (s3[i+j]==s2[j] and D[i][j-1]) using 1 indexing for simplicity
+        D = [[False for i in range(l2+1)] for j in range(l1+1)]
+        D[0][0] = True
+        for i in range(l1+1):
+            for j in range(l2+1):
+                if i==0 and j>0:
+                    D[i][j] = s3[i+j-1]==s2[j-1] and D[i][j-1]
+                elif j==0 and i>0:
+                    D[i][j] = s3[i+j-1]==s1[i-1] and D[i-1][j]
+                elif i>0 and j>0:
+                    D[i][j] = (s3[i+j-1]==s2[j-1] and D[i][j-1]) or (s3[i+j-1]==s1[i-1] and D[i-1][j])
                 
-        return True
-
-
+        return D[-1][-1]
