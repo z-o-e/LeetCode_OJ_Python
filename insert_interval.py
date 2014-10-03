f4462c87c3dc8b61e6de91b822a5b9162cde9fa2
Zoe Zhang <hyzhang22@gmail.com>
2014-10-03 16:06:01 -0700

    array ops

--- insert_interval.py
+++ insert_interval.py
@@ -9,26 +9,42 @@ class Solution:
     # @param newInterval, a Interval
     # @return a list of Interval
     def insert(self, intervals, newInterval):
-        if not newInterval:
-            return intervals
         if not intervals:
             return [newInterval]
         
-        res = []
-        while intervals!=[]:
-            if newInterval.start > intervals[0].end:
-                res += [intervals[0]] + [newInterval]
-                
-            elif newInterval.end < intervals[0].start:
-                res += [newInterval] + [intervals[0]]
-                
+        # idea: insert newInterval, keeping intervals sorted by first element --> merge interval
+        if newInterval.start <= intervals[0].start:
+            # insert at head
+            intervals =  [newInterval]+intervals
+            startIdx = 1
+        elif newInterval.start >= intervals[-1].start:
+            # insert at tail
+            intervals.append(newInterval)
+            startIdx = len(intervals)-1
+        else:
+            # insert in the middle
+            for i in range(len(intervals)):
+                if newInterval.start < intervals[i].start:
+                    intervals = intervals[:i] + [newInterval] + intervals[i:]
+                    startIdx = i
+                    break
+                    
+        return self.merge(intervals, startIdx)
+   
+    def merge(self, intervals, startIdx):
+        # check and merge intervals starting from startIdx
+        i = startIdx
+        while i < len(intervals):
+            if intervals[i].start <= intervals[i-1].end:
+                # merge
+                if intervals[i].end >= intervals[i-1].end:
+                    intervals[i].start = intervals[i-1].start
+                    del intervals[i-1]
+                else:
+                    del intervals[i]
             else:
-                newInterval.start = min(intervals[0].start, newInterval.start)
-                newInterval.end = max(intervals[0].end, newInterval.end)
-                res += [newInterval] + intervals[1:]
-                break
+                i += 1
                 
-            intervals = intervals[1:]
-            
-        return res
-            
+        return intervals
+        
+                    
